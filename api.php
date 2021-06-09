e45ed5c818e27753ae1729cdb2f2fd5920455383
GianniCampana <campanagianni97@gmail.com>
2021-06-09 22:27:33 +0200

    DISCHI-GENRES

--- api.php
+++ api.php
@@ -2,9 +2,35 @@
 
 require_once __DIR__ . '/data/db.php';
 
+$genres = [];
+$albums = empty($_GET['genre']) || $_GET['genre'] === 'all' ? $database  : [];
+
+
+
+foreach($database as $album){
+    if(!in_array($album['genre'], $genres)){ 
+        $genres[] = $album['genre'];
+    }
+}
+
+if(count($albums) === 0){
+    foreach($database as $album){
+        if($album['genre'] === $_GET['genre']){
+            $albums[] = $album;
+        }
+    }
+}
+
+
+
+$response = [
+    'albums' => $albums,
+    'genres' => $genres
+];
+
 
 header('Content-Type: application/json');
 
-echo json_encode($database);
+echo json_encode($response);
 
-?>
+?> 
--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const app = new Vue({
 
 
-el: '#app',
-
-data:{
-        albums:[],
-        apiUrl: 'http://localhost:8888/php-ajax-dischi/api.php',
-
-},
-methods:{
-
-    getAPI(){
-        axios.get(this.apiUrl)
-        .then(res => {
-            this.albums=res.data;
-            console.log(this.albums);
-        }).catch(err =>{
-            console.log(err);
-        })
+    el: '#app',
+    
+    data:{
+            albums:[],
+            genres:[],
+            genreToSearch: 'all',
+            apiUrl: 'http://localhost:8888/php-ajax-dischi/api.php',
+    
+    },
+    methods:{
+    
+        getAPI(){
+            axios.get(this.apiUrl,{
+                params:{
+                    genre: this.genreToSearch
+                }
+            })
+            .then(res => {
+                this.albums=res.data.albums;
+                this.genres=res.data.genres;
+                console.log(res.data.albums);
+                console.log(this.genres);
+            }).catch(err =>{
+                console.log(err);
+            })
+        }
+      
+    },
+    
+    created(){
+        this.getAPI();
+    
     }
-  
-},
-
-created(){
-    this.getAPI();
-
-}
-
-});
+    
+    });
